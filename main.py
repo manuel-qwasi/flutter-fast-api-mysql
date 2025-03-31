12bf3aabcab447b04523f7efbf5864491b1d9a99
ProximaAitch <emmanuelanokye1@gmail.com>
2025-03-31 15:08:00 +0000

    removed local env from repo

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import os
 # Load environment variables
 load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+REFRESH_SECRET_KEY = os.getenv("REFRESH_SECRET_KEY")
+
 # Create database tables
 Base.metadata.create_all(bind=engine)
 
